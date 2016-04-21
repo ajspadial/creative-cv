00cee3378bab702af675446ae9300532092b0a90
Antonio Jesús Sánchez Padial <antonio.sanchez@inia.es>
2016-04-21 15:39:27 +0200

    First commit

---
+++ memoria.js
@@ -0,0 +1,105 @@
+var memoria="#MEMORIA
+
+**LOTK.ca** es un ecosistema virtual interactivo cuyos habitantes se generan por colaboración en red, su actividad se regula en base a parámetros provenientes del entorno urbano y se representa bien por medio de la fachada multimedia del Medialab-Prado, bien a través de Internet.
+
+Este proyecto ha sido nombrado en honor al matemático estadounidense  **Alfred Lotka**, que junto al italiano Vito Volterra planteó y resolvió las ecuaciones que describen la interacción entre las poblaciones de depredadores y presas. 
+
+##Objetivo
+
+El objetivo final de **LOTK.ca** es conseguir una representación artística de un esfuerzo colectivo distribuido, capaz de desarrollarse alrededor de las restricciones técnicas de la fachada, y semánticas de los recursos provistos por el ecosistema virtual.
+
+En paralelo a este objetivo final **LOTK.ca** se plantea una serie de objetivos secundarios:
+
+* divulgación científica, de la fragilidad de los ecosistemas tanto debido a los cambios de sus parámetros sustentatorios (clima, polución, ...) como de la aparición y desaparición de agentes.
+
+* invitación a participar en el medio, esperamos que mediante la exposición conjunta de la fachada y de la comunidad web, se despierte en algunos sectores del público un interés por aportar su trabajo al ecosistema, y de investigar como pueden hacerlo cambiar y desarrollarse.
+
+* divulgación tecnológica, participar en **LOTK.ca** no implica necesariamente programar, pero deja abierta esta posibilidad al basarse en Processing.
+
+##Plan de actuación
+
+El proyecto se desarrollará en base a tres acciones:
+
+* talleres
+
+* hackatones
+
+* evaluación
+
+En los **talleres** se sentarán las bases del proyecto, compartiendo con el público interesado las herramientas de desarrollo para el ecosistema virtual, invitándoles así a su participación.
+
+El taller es una herramienta clave del ecosistema del Medialab-Prado, y queremos potenciar su importancia colocándolo en el lugar central de nuestro desarrollo.
+
+El proyecto **lotk.ca** reservará una parte de estos talleres a una o varias charlas divulgativas, por parte de algún científico, técnico o artista, 
+¡o las tres cosas! cuya línea de trabajo sea convergente con los objetivos del proyecto.
+
+Los **hackatones** (de *hacker* y maratón) son reuniones de desarrollo, aún menos estructuradas que el taller, donde los colaboradores deciden sobre qué parte del proyecto actuar y lo desarrollan en un tiempo determinado, normalmente un fin de semana.
+
+Por último, no queremos cerrar este proyecto sin realizar una **evaluación** del mismo, analizando tanto los ecosistemas virtuales creados, como los ecosistemas reales de desarrolladores y público general del proyecto.
+
+##Fases de desarrollo
+
+* Desarrollo del sitio web de presentación.
+	* Blog de desarrollo
+	* Documentación técnica
+	* Incluye elementos de comunidad como foros, lista de correo, etc.
+
+* Desarrollo y pruebas de la estructura del ecosistema virtual.
+	* Puede incluir un primer hackaton si se ha conseguido hacer despegar la comunidad de desarrolladores a través de la web.
+
+* Talleres
+	* De programación en Processing
+	* De vida artificial
+	* De presentación del ecosistema virtual para desarrolladores
+
+	Estos talleres pueden plantearse por separado, o como un todo. El fruto final de estos talleres deberían ser las primeras comunidades virtuales coexistiendo en el ecosistema virtual.
+
+* Depuración. En esta fase del proyecto se empezarán a detectar inconvenientes y limitaciones técnicas, no predecible hasta que empiece a ser usado fuera del ámbito de los desarrolladores originales del sistema. Requerirá diversas correcciones y ajustes.
+
+* Hackatones ecológicos. Se plantearán uno o dos hackatones basados en desarrollar un ecosistema virtual o un aspecto del mismo. Estos ecosistemas se podrán basar en la escasez o sobreabundancia de determinado recurso, o bien en fomentar la interacción del ecosistema virtual a las condiciones urbanas, especificamente de la plaza de la fachada del medialab.
+
+* Evaluación del proyecto
+
+##Metodología
+
+
+##Líneas de investigación
+
+* Análisis de las herramientas de desarrollo (programación) colectiva a través de la web.
+
+* Análisis de las comunidades de desarrollo constituidas en torno a un programa de investigación artístico-científico-tecnológica.
+
+* Análisis de la interacción on line y presencial (fachada) del público ante el trabajo colaborativo.
+
+##Público objetivo
+
+El público objetivo de las acciones de este proyecto es muy variado:
+
+* Comunidad artística interesada en computación. Este es un público muy presente en el media lab, al que el proyecto no solo puede ayudar a investigar algunos aspectos computacionales en los que puede estar interesados, sino que además se verá retroalimentado por su experiencia, colaboración, etc. Este público es alcanzado a través de la actividad en el Medialab
+
+* Comunidad científico-tecnológica. Existe una comunidad interesada en los ecosistemas virtuales que proviene tanto del mundo científico de la biología, como del tecnológico. Para alcanzar este público habría que dirigirse a los ambientes que pueda frecuentar, como facultades y escuelas universitarias, asociaciones y páginas de internet especializadas.
+
+* Divulgación científica. La ecología y la programación son dos de las ramas de la ciencia y la tecnología que más interés despiertan en el público en general. Este proyecto puede servir para atraer  al público interesado en uno de estos temas hasta al otro, poniendo en relevancia los puntos de conexión que los unen.
+
+##Justificación personal de la conveniencia de llevarlo a cabo
+##Entidades colaboradoras
+
+(Juegos en pantalla Gigante)[http://juegosenpantallagigante.com] es un colectivo emergente cuyo objetivo
+es el desarrollo de juegos interactivos basados en pantallas móviles de gran tamaño, de modo que ponen esta tecnología
+cultural y lúdica al alcance de comunidades, como las rurales, normalmente excluídas del acceso a las mismas.
+
+**Juegos en pantalla Gigante** colaborará aportando su experiencia en el desarrollo de interfaces para grandes pantallas,
+ usando sistemas de visión por computador.
+
+Actualmente, el proyecto está buscando entidades colaboradoras en el ámbito de las asociaciones culturales o científicas que puedan estar interesadas en compartir sus objetivos.
+
+##Plazos de realización
+* Último trimestre de 2012
+> * Desarrollo del sitio web
+* Primer trimestre de 2013
+> * Desarrollo de la plataforma del ecosistema virtual
+* Segundo trimestre de 2013
+> * Talleres y hackatones en función del calendario de las entidades colaboradoras
+* Fin del segundo trimestre de 2013
+> * Presentación y discusión de los resultados obtenidos.
+";
